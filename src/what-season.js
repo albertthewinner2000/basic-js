3efef26e64da6fac9bae2edd24409430c95adaad
albertthewinner2000 <nalbert2012@ya.ru>
2020-09-24 17:47:37 +0300

    feat:add what season function

--- src/what-season.js
+++ src/what-season.js
@@ -2,47 +2,27 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
   if (date === undefined) {
-    return 'Unable to determine the time of year!'
+    return "Unable to determine the time of year!";
   }
   if (!Date.prototype.isPrototypeOf(date)) {
     throw Error;
   }
-  let month = date.getMonth();
-  if (month === 11 || month === 0 || month === 1) {
-    return 'winter'
-  }
-  if (month >= 2 && month <= 4) {
-    return 'spring'
-  }
-  if (month >= 5 && month <= 7) {
-    return 'summer'
-  }
-  if (month >= 8 && month <= 10) {
-    return 'autumn'
-  }
-};
-
-function getSeason(date) {
-  if (date === undefined) {
-    return 'Unable to determine the time of year!'
-  }
-  if (!Date.prototype.isPrototypeOf(date)) {
+  if (Object.keys(date).length !== 0) {
     throw Error;
   }
   let month = date.getMonth();
   if (month === 11 || month === 0 || month === 1) {
-    return 'winter'
+    return "winter";
   }
   if (month >= 2 && month <= 4) {
-    return 'spring'
+    return "spring";
   }
   if (month >= 5 && month <= 7) {
-    return 'summer'
+    return "summer";
   }
   if (month >= 8 && month <= 10) {
-    return 'autumn'
+    return "autumn";
   }
 };
 
-let date  = new Date(2000, 11, 02);
-console.log(getSeason(date))
+
