aed871cd06433adae0a5dd55a0dd763b7b2553d6
albertthewinner2000 <nalbert2012@ya.ru>
2020-09-27 01:01:22 +0300

    feat:completed a few tasks
    trasnform array was completed

--- src/dream-team.js
+++ src/dream-team.js
@@ -11,28 +11,7 @@ module.exports = function createDreamTeam(members) {
   return name.sort().join("").toUpperCase();
 }
 
-function createDreamTeam(members) {
-  if (!Array.isArray(members)) return false;
-  let name = [];
-  for (let member of members) {
-    if (typeof member === "string") {
-      name.push(member.trim()[0].toUpperCase());
-    }
-  }
-  return name.sort().join("");
-}
 
-console.log(createDreamTeam([
-  ['David Abram'],
-  ['Robin Attfield'],
-  'Thomas Berry',
-  ['Paul R.Ehrlich'],
-  'donna Haraway',
-  ' BrIaN_gOodWiN  ',
-  {
-    0: 'Serenella Iovino'
-  },
-  'Erazim Kohak',
-  '  val_plumwood',
-]))
+
+
 
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+const transform = function (arr) {
+  if (!Array.isArray(arr)) throw Error;
+  if (
+    !(
+      arr.includes("--discard-next") ||
+      arr.includes("--double-next") ||
+      arr.includes("--discard-prev") ||
+      arr.includes("--double-prev")
+    )
+  ) {
+    return arr;
+  }
+  let newArr = [...arr];
+  let element = newArr[0];
+  for (let index = 0; index < newArr.length; index++) {
+    element = newArr[index];
+    if (
+      element === "--discard-next" ||
+      element === "--discard-prev" ||
+      element === "--double-next" ||
+      element === "--double-prev"
+    ) {
+      //action
+      switch (element) {
+        case "--discard-prev":
+          newArr[index - 1] = undefined;
+          newArr[index] = undefined;
+          break;
+        case "--discard-next":
+          newArr[index + 1] = undefined;
+          newArr[index] = undefined;
+          break;
+        case "--double-prev":
+          newArr[index] = newArr[index - 1];
+          break;
+        case "--double-next":
+          newArr[index] = newArr[index + 1];
+          break;
+      }
+    } 
+  }
+  return newArr.filter((value) => value !== undefined);
 };
+
+module.exports = transform;
+
+
